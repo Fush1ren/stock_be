af0689de0284d8fda95c06bcc7c0ba0b511ebf56
Fush1ren <nightingale.ox@gmail.com>
2025-05-04 15:18:06 +0700

    fix: change category function on route

--- src/api/v1/category/category.route.ts
+++ src/api/v1/category/category.route.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { verifyToken } from '../../../middleware';
-import { createCategory } from './category.controller';
+import { createCategory, getCategory } from './category.controller';
 
 const categoryRouter = Router();
 
 categoryRouter.post('/', verifyToken, createCategory);
-categoryRouter.get('/', verifyToken, createCategory); // Assuming you want to use the same controller for GET as well
+categoryRouter.get('/', verifyToken, getCategory); // Assuming you want to use the same controller for GET as well
 
 export default categoryRouter;
