162e58a091de5a526574c2b7bff33455fd7c2d02
Fush1ren <nightingale.ox@gmail.com>
2025-06-01 20:10:44 +0700

    feat: add sorting in user controller

--- src/api/types/data.type.ts
+++ src/api/types/data.type.ts
@@ -2,10 +2,17 @@ export interface IQuery {
     take?: number;
     skip?: number;
     select?: any;
+    orderBy?: any;
 }
+
 export interface InsertUpdateQuery {
     take?: any;
     skip?: any;
     select?: any;
     data: any;
+}
+
+export interface ISortParams {
+    sortBy?: string
+    sortOrder?: string | number
 }
