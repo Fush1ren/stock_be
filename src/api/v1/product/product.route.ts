edb9dceaa6e6fe39d5cdc30d3b93b0da7710ac37
Fush1ren <nightingale.ox@gmail.com>
2025-06-02 22:58:40 +0700

    fix: update endpoint product

--- src/api/v1/product/product.route.ts
+++ src/api/v1/product/product.route.ts
@@ -6,10 +6,10 @@ import { deleteBrand } from '../brand/brand.controller';
 const productRoute = Router();
 
 productRoute.get('/', verifyToken, getAllProducts);
+productRoute.get('/next-index', verifyToken, getNextIndex);
 productRoute.get('/:id', verifyToken, getProductById);
 productRoute.post('/', verifyToken, createProduct);
 productRoute.put('/:id', verifyToken, updateProduct); // Assuming updateProduct is similar to createProduct
 productRoute.delete('/', verifyToken, deleteBrand);
-productRoute.get('/next-index', verifyToken, getNextIndex);
 
 export default productRoute;
