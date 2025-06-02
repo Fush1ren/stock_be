46d38cbd448445aa98407d9200882d0a86d463da
Fush1ren <nightingale.ox@gmail.com>
2025-06-02 22:21:46 +0700

    feat: add getProductById endpoint

--- src/api/v1/product/product.route.ts
+++ src/api/v1/product/product.route.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { createProduct, getAllProducts, getNextIndex, updateProduct } from './product.controller';
+import { createProduct, getAllProducts, getNextIndex, getProductById, updateProduct } from './product.controller';
 import { verifyToken } from '../../../middleware';
 import { deleteBrand } from '../brand/brand.controller';
 
 const productRoute = Router();
 
 productRoute.get('/', verifyToken, getAllProducts);
+productRoute.get('/:id', verifyToken, getProductById);
 productRoute.post('/', verifyToken, createProduct);
 productRoute.put('/:id', verifyToken, updateProduct); // Assuming updateProduct is similar to createProduct
 productRoute.delete('/', verifyToken, deleteBrand);
